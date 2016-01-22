a4f29c63d65d21d096217895e6c4ca7f3e1cf99d
julfy <julfy777@gmail.com>
2016-01-22 11:59:06 -0800

    transform and load

---
+++ load.py
@@ -0,0 +1,102 @@
+import gzip
+import os
+import numpy
+import tensorflow as tf
+from numpy import genfromtxt
+
+def extract_data (filename):
+    data = genfromtxt(filename, delimiter=',')
+    return data
+
+class DataSet(object):
+
+  def __init__(self, inputs, labels, dtype=tf.float32):
+    """Construct a DataSet.
+    one_hot arg is used only if fake_data is true.  `dtype` can be either
+    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+    `[0, 1]`.
+    """
+    dtype = tf.as_dtype(dtype).base_dtype
+
+    assert inputs.shape[0] == labels.shape[0], ('inputs.shape: %s labels.shape: %s' % (inputs.shape,
+                                                                                       labels.shape))
+    self._num_examples = inputs.shape[0]
+
+    self._inputs = inputs
+    self._labels = labels
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def inputs(self):
+    return self._inputs
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  def next_batch(self, batch_size):
+    """Return the next `batch_size` examples from this data set."""
+    start = self._index_in_epoch
+    self._index_in_epoch += batch_size
+    if self._index_in_epoch > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Shuffle the data
+      perm = numpy.arange(self._num_examples)
+      numpy.random.shuffle(perm)
+      self._inputs = self._inputs[perm]
+      self._labels = self._labels[perm]
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size
+      assert batch_size <= self._num_examples
+    end = self._index_in_epoch
+    return self._inputs[start:end], self._labels[start:end]
+
+
+def read_data_sets(dataf, labelsf, validation, test, dtype=tf.float32):
+  class DataSets(object):
+    pass
+  data_sets = DataSets()
+
+  data = extract_data(dataf)
+  labels = extract_data(labelsf)
+
+  size = len(labels)
+
+  validation_s = int(size * validation)
+
+  test_s = int(size * test)
+
+  training_s = size - validation_s - test_s
+
+  print "Read: %d examples", size
+  print "  Training set =   ", training_s
+  print "  Validation set = ", validation_s
+  print "  Test set =       ", test_s
+
+  train_data = data[:training_s]
+  train_labels = labels[:training_s]
+
+  validation_data = data[training_s:training_s+validation_s]
+  validation_labels = labels[training_s:training_s+validation_s]
+
+  test_data = data[training_s+validation_s:training_s+validation_s+test_s]
+  test_labels = labels[training_s+validation_s:training_s+validation_s+test_s]
+
+  data_sets.train = DataSet(train_data, train_labels, dtype=dtype)
+  data_sets.validation = DataSet(validation_data, validation_labels, dtype=dtype)
+  data_sets.test = DataSet(test_data, test_labels, dtype=dtype)
+
+  return data_sets
+
+read_data_sets ('data','labels',0.3,0.1)
--- transform.py
+++ transform.py
@@ -1,6 +1,7 @@
-import json
+import simplejson as json
 import glob
 import re
+import csv
 from urlparse import urlparse
 from pprint import pprint
 
@@ -9,8 +10,20 @@ def check_match (s1, s2):
     s2l = re.findall(r"[\w']+", s2.lower())
     return len(set(s1l) & set(s2l))
 
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key):byteify(value) for key,value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
 def process(jsn):
     i = 0
+    data = []
+    labels = []
     for res in jsn['urls']:
         if i < 10:
             label = 1
@@ -19,17 +32,26 @@ def process(jsn):
         common_content = check_match (jsn['keyword'], res['content'])
         common_url = check_match (jsn['keyword'],urlparse(res['url']).path+urlparse(res['url']).hostname)
         url_len = len(re.sub(r"www\.","", urlparse(res['url']).hostname))
-        print [res['ahrefs_rank'], res['domain_rating'],common_url,common_content,url_len,label]
+        input = [res['ahrefs_rank'], res['domain_rating'],common_url,common_content,url_len]
+        data.append(input)
+        labels.append([label])
         i += 1
-        
+    return data,labels
 
-with open('/home/julfy/work/results/6004ba3055b15f6b11f79bade79dcc31') as json_data:
-    d = json.load(json_data)
-    json_data.close()
-    process(d)
+def transform_data(dir, outd, outl):
+    filelist = glob.glob(dir)
+    for filename in filelist:
+        print "Reading ", filename
+        with open(filename) as json_data:
+            d = json.load(json_data)
+            json_data.close()
+            data,labels = process(d)
+            with open(outd, 'ab') as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+            with open(outl, 'ab') as f:
+                writer = csv.writer(f)
+                writer.writerows(labels)
+    print "Processed %d files", len(filelist)
 
-    
-# filelist = glob.glob("/home/julfy/work/results/*")
-
-# for filename in filelist:
-#     j += 1
+transform_data ("/home/bogdan/Downloads/results/*", 'data', 'labels')
